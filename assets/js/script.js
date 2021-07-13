d490a91b46dde943a3a742fb193e17f82b548d94
Xander <xanschwarz@gmail.com>
2021-07-13 13:44:51 -0700

    Add color coding to timeblocks based on current time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,27 @@
-var momentDay = moment().format('dddd, MMMM Do');
-$('#currentDay').text(momentDay)
+// Set moment variable to be used throughout.
+var theMoment = moment();
+$('#currentDay').text(theMoment.format('dddd, MMMM Do'))
+
+// Preliminarily set desciption areas to past coloring. Function will change as appropriate to present or future coloring.
+var descriptionEl = $('.description');
+descriptionEl.addClass('past');
+
+// Function for color coding boxes based on current time of day.
+function colorCoding() {
+    var momentHours = parseInt(theMoment.format('H'));
+    var rowTimeCheck = $('.row');
+    
+    // Step through rowTimeCheck, for each id match against momentHours and change color accordingly.
+    for (i = 0; i < rowTimeCheck.length; i++) {
+        var rowTime = parseInt($(rowTimeCheck[i]).attr('id'));
+
+        if (momentHours == rowTime) {
+            $(rowTimeCheck[i]).children('.description').addClass('present');
+        }
+        else if (momentHours < rowTime) {
+            $(rowTimeCheck[i]).children('.description').addClass('future');
+        }
+    }
+
+}
+colorCoding();
