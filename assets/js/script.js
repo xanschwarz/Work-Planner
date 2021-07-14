085dda111d04f0fb6bbf0c37676b41d2a4de8c1b
Xander <xanschwarz@gmail.com>
2021-07-13 22:26:13 -0700

    Final touches and addition of images for ReadMe.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
+// Initialize the timeblock save buttons and description boxes, and a moment variable.
 var saveButton = $('.saveBtn');
-
-// Set moment variable to be used throughout. Display the day and date above the planner.
+var descriptionEl = $('.description');
 var theMoment = moment();
+
+// Display the current day and date above the planner.
 $('#currentDay').text(theMoment.format('dddd, MMMM Do'));
 
-// Preliminarily set desciption areas to past coloring. Function will change as appropriate to present or future coloring.
-var descriptionEl = $('.description');
+// Preliminarily set desciption areas to past coloring.
 descriptionEl.addClass('past');
 
-// Function for color coding boxes based on current time of day.
-function colorCoding() {
-    var momentHours = parseInt(theMoment.format('H'));
+// Function for color coding timeblocks based on current time of day and whether time is past, present,
+// or future.
+$(function colorCoding() {
+    // Variables for current moment in hours and the timeblock rows.
+    var momentHours = parseInt(theMoment.format('H'))-8;
     var rowTimeCheck = $('.row');
     
-    // Step through rowTimeCheck, for each id match against momentHours and change color accordingly (by class).
+    // Step through rowTimeCheck, for each rows assigned time (as identified by id) match against momentHours
+    // and change color accordingly (by class).
     for (i = 0; i < rowTimeCheck.length; i++) {
         currentRow = $(rowTimeCheck[i]);
         var rowTime = parseInt(currentRow.attr('id'));
@@ -26,17 +30,19 @@ function colorCoding() {
         }
     }
 
-};
-colorCoding();
+});
 
+// If the save button is clicked, get the time id of the row it's a part of and the input to the textarea and
+// save them in local storage.
 saveButton.on('click', function(event) {
     var clicked = $(event.target);
     var targetHour = clicked.closest('section').attr('id');
-    var scheduleItem = JSON.stringify(clicked.siblings('.description').val());
+    var scheduleItem = (clicked.siblings('.description').val());
     localStorage.setItem(targetHour, scheduleItem);
 });
 
-function init() {
+// Display any locally stored textarea input in the row they were input to on page loading.
+$(function init() {
     $('#9 .description').val(localStorage.getItem('9'));
     $('#10 .description').val(localStorage.getItem('10'));
     $('#11 .description').val(localStorage.getItem('11'));
@@ -48,5 +54,4 @@ function init() {
     $('#17 .description').val(localStorage.getItem('17'));
     $('#18 .description').val(localStorage.getItem('18'));
     $('#19 .description').val(localStorage.getItem('19'));
-}
-init();
+});
